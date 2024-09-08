16ceb7995f50da7f646d54c83d866916c62c69ae
szoha2095 <szoha2095@gmail.com>
2024-09-08 10:09:30 +0530

    Full Name code changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FullName from "./FullName";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FullName />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FullName.jsx
@@ -0,0 +1,49 @@
+import { React, useState } from "react";
+const FullName = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setFullName(`${firstName} ${lastName}`);
+  };
+
+  const handleFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastName = (event) => {
+    setLastName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={handleFirstName}
+          required
+        />{" "}
+        <br />
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={handleLastName}
+          required
+        />{" "}
+        <br />
+        <button type="submit">Submit</button>
+        <p className="fullName">Full Name: {fullName}</p>
+      </form>
+    </div>
+  );
+};
+
+export default FullName;
